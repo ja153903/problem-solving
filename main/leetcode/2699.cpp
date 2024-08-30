4bb87ae6a235d821dad8903e3ad112cc57a3dc83
ja153903 <jaimeabbariao@gmail.com>
2024-08-30 17:03:34 -0400

    adding template for daily problem (haven't solved it yet)

---
+++ main/leetcode/2699.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+class Solution {
+  public:
+    std::vector<std::vector<int>>
+    modifiedGraphEdges(int n, std::vector<std::vector<int>>& edges, int source,
+                       int destination, int target) {}
+};
